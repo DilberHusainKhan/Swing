800ef33e8d5b3530c40a10c04c336bb03d21f0ff
dilberhusainkhan <dilberjmi@gmail.com>
2023-01-03 16:51:30 +0530

    J Label intro complete

--- B_JLabelIntro.java
+++ B_JLabelIntro.java
@@ -24,7 +24,7 @@ public class B_JLabelIntro {
         JLabel Mobile = new JLabel("Student Mobile");
         
         // set locatio in the Container 
-        Sign_Up.setBounds(200,50,100,100);
+        Sign_Up.setBounds(200,80,100,100);
         Id.setBounds(100,150,200,50);
         Name.setBounds(100,200,200,50);
         Add.setBounds(100,250,200,50);
@@ -37,15 +37,31 @@ public class B_JLabelIntro {
         Add.setFont(Normal);
         Mobile.setFont(Normal);
         
+        // Set Frame Icon
+        ImageIcon icon = new ImageIcon("./signup.png");
+        frame.setIconImage(icon.getImage());
+
+        
+        // To add image to the Container 
+        ImageIcon signup =new ImageIcon("./signup.png");
+        JLabel image = new JLabel(signup,JLabel.CENTER);
+        image.setBounds(0,20,500,100);
+        // Image at bottom part
+        JLabel image1 = new JLabel("Sign UP",signup,JLabel.CENTER);
+        image1.setBounds(0,450,500,signup.getIconHeight());
+
+
+
         // add lable to the Container 
         c.add(Sign_Up);
         c.add(Id);
         c.add(Name);
         c.add(Add);
         c.add(Mobile);
+        c.add(image);
+        c.add(image1);
         
 
-
-        // frame.setResizable(false);
+        frame.setResizable(false);
     }
 }
