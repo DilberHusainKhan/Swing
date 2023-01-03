007551fada38d2bae96afeffa8115409d5aa257c
dilberhusainkhan <dilberjmi@gmail.com>
2023-01-03 13:17:22 +0530

    Important method about JFrame

--- MyFirstJFrame.java
+++ MyFirstJFrame.java
@@ -1,3 +1,6 @@
+import java.awt.Color;
+import java.awt.Container;
+
 import javax.swing.*;
 public class MyFirstJFrame {
     public static void main(String[] args) {
@@ -6,9 +9,12 @@ public class MyFirstJFrame {
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
         // frame.setSize(700,500);
         // frame.setLocation(200,50);
-        frame.setBounds(50,100,500,500);
+        frame.setBounds(50,100,1000,500);
         frame.setTitle("My First Swing");
         ImageIcon icon = new ImageIcon("pic1.jpg");
-        icon.setImage(icon.getImage());
+        frame.setIconImage(icon.getImage());
+        Container c=frame.getContentPane();
+        c.setBackground(Color.DARK_GRAY);
+        frame.setResizable(false);
     }
 }
